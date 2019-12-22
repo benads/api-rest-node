f7f431087385a2db3041d32c5778adb4d5eb72da
Benjamin <benjamin.adida@hetic.net>
2019-12-22 19:53:11 +0100

    add morgan package for debug

--- app.router.js
+++ app.router.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
 
 app.get('/api', (req, res) => {
   res.send('Root API')
+})
+
+app.get('/api/books/:id', (req, res) => {
+  res.send(req.params)
 })
 
 app.listen(8080, () => {

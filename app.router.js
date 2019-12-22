a0cffd2c24daeb6dcf26061e7f389dfab26fe78e
Benjamin <benjamin.adida@hetic.net>
2019-12-22 20:06:27 +0100

    get method for the API

--- app.router.js
+++ app.router.js
@@ -2,14 +2,33 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
-app.use(morgan('dev'));
+app.use(morgan('dev')); //middleware
 
-app.get('/api', (req, res) => {
-  res.send('Root API')
+const members = [
+  {
+    id: 1,
+    name: 'Benjamin'
+  },
+  {
+    id: 2,
+    name: 'Johnny'
+  },
+  {
+    id: 3,
+    name: 'Doe'
+  },
+]
+
+app.get('/api/members/:id', (req, res) => {
+  res.send(members[(req.params.id)])
 })
 
-app.get('/api/books/:id', (req, res) => {
-  res.send(req.params)
+app.get('/api/members', (req, res) => {
+  if(req.query.max != undefined && req.query.max > 0) {
+    res.send(members.slice(0, req.query.max))
+  } else {
+    res.send(members)
+  }
 })
 
 app.listen(8080, () => {

196040491011d6efc3810c5f0cf7936e3021dbed
Benjamin <benjamin.adida@hetic.net>
2019-12-22 23:15:17 +0100

    post method on api, need to fix bug about header

--- app.router.js
+++ app.router.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser')
+const statusMessage = require('functions/statusMessage')
 
 app.use(morgan('dev')); //middleware
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({extended: true})); // for parsing application/json
 
 const members = [
   {
@@ -30,6 +34,44 @@ app.get('/api/members', (req, res) => {
     res.json(members)
   }
 })
+
+app.post('/api/members', (req, res) => {
+
+  if(req.body.name) {
+
+    let sameName = false;
+    for(let i = 0; i < members.length; i++) {
+
+      if(members[i].name == req.body.name) {
+
+        res.json(statusMessage.error('Name already taken'))
+        
+        sameName = true;
+
+        break;  
+      }
+    }
+
+    if (sameName) {
+
+      res.json(statusMessage.error('No name value'))
+
+    } else {
+
+      let member = 
+      {
+        id: members.length + 1,
+        name: req.body.name
+      } 
+  
+      members.push(member)
+  
+      res.json(statusMessage.success(member))
+
+    }
+  } 
+})
+
 
 app.listen(8080, () => {
   console.log('started on port 8080')
